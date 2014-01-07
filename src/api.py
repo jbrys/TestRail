5e2b91086aacb5955f7d3b62b5b1ec632b706f33
Jeff Brys <jeff.brys@grindr.com>
2014-01-07 00:22:32 -0800

    git requests are _almost_ complete

--- src/api.py
+++ src/api.py
@@ -1,10 +1,14 @@
 """api.py: api functions for testrail api wrapper"""
 import requests
-
+import json
 import settings
 
 # Will error without setting the header
 headers = {"Content-Type": "application/json"}
+
+def pretty(payload):
+    print json.dumps(payload, sort_keys=True,
+                     indent=4, separators=(',', ':'))
 
 def get_case(id):
     """Returns an existing test case."""
@@ -61,21 +65,21 @@ def delete_case(id):
                         auth=(settings.USR, settings.PWD), headers=headers)
     return req.json()
 
-def get_milestone(id):
+def get_milestone(milestone_id):
     """Returns an existing milestone."""
-    req = requests.get(settings.URL + 'get_milestone/' + str(id),
+    req = requests.get(settings.URL + 'get_milestone/' + str(milestone_id),
                        auth=(settings.USR, settings.PWD), headers=headers)
     return req.json()
 
-def get_milestones(id):
+def get_milestones(project_id):
     """Returns a list of milestones for a project."""
-    req = requests.get(settings.URL + 'get_milestones/' + str(id),
+    req = requests.get(settings.URL + 'get_milestones/' + str(project_id),
                        auth=(settings.USR, settings.PWD), headers=headers)
     return req.json()
 
-def get_project(id):
+def get_project(project_id):
     """Returns an existing project."""
-    req = requests.get(settings.URL + 'get_project/' + str(id),
+    req = requests.get(settings.URL + 'get_project/' + str(project_id),
                        auth=(settings.USR, settings.PWD), headers=headers)
     return req.json()
 
@@ -85,56 +89,75 @@ def get_projects():
                        auth=(settings.USR, settings.PWD), headers=headers)
     return req.json()
 
-def get_plan(id):
+def get_plan(plan_id):
     """Returns an existing test plan."""
-    req = requests.get(settings.URL + 'get_plan/' + str(id),
+    req = requests.get(settings.URL + 'get_plan/' + str(plan_id),
                        auth=(settings.USR, settings.PWD), headers=headers)
     return req.json()
 
-def get_plans(id):
+def get_plans(project_id):
     """Returns a list of test plans for a project."""
-    req = requests.get(settings.URL + 'get_plans/' + str(id),
+    req = requests.get(settings.URL + 'get_plans/' + str(project_id),
                        auth=(settings.USR, settings.PWD), headers=headers)
     return req.json()
 
-def get_run(id):
+def get_run(run_id):
     """Returns an existing test run."""
-    req = requests.get(settings.URL + 'get_run/' + str(id),
+    req = requests.get(settings.URL + 'get_run/' + str(run_id),
                        auth=(settings.USR, settings.PWD), headers=headers)
     return req.json()
 
-def get_runs(id):
+def get_runs(project_id):
     """Returns a list of test runs for a project. Only returns those test runs that are not part of a test plan."""
-    req = requests.get(settings.URL + 'get_runs/' + str(id),
+    req = requests.get(settings.URL + 'get_runs/' + str(project_id),
                        auth=(settings.USR, settings.PWD), headers=headers)
     return req.json()
 
-def get_section(id):
+def get_section(section_id):
     """Returns an existing section."""
-    req = requests.get(settings.URL + 'get_section/' + str(id),
+    req = requests.get(settings.URL + 'get_section/' + str(section_id),
                        auth=(settings.USR, settings.PWD), headers=headers)
     return req.json()
 
-def get_sections(id):
+def get_sections(suite_id):
     """Returns a list of sections for a project and test suite."""
-    req = requests.get(settings.URL + 'get_sections/' + str(id),
+    req = requests.get(settings.URL + 'get_sections/' + str(suite_id),
                        auth=(settings.USR, settings.PWD), headers=headers)
     return req.json()
 
-def get_test(id):
+def get_test(test_id):
     """Returns an existing test."""
-    req = requests.get(settings.URL + 'get_test/' + str(id),
+    req = requests.get(settings.URL + 'get_test/' + str(test_id),
                        auth=(settings.USR, settings.PWD), headers=headers)
     return req.json()
 
-def get_tests(id):
+def get_tests(run_id):
     """Returns a list of tests for a test run."""
-    req = requests.get(settings.URL + 'get_tests/' + str(id),
+    req = requests.get(settings.URL + 'get_tests/' + str(run_id),
                        auth=(settings.USR, settings.PWD), headers=headers)
     return req.json()
 
-def get_results(id):
+def get_results(test_id):
     """Returns a list of test results for a test."""
-    req = requests.get(settings.URL + 'get_results/' + str(id),
+    req = requests.get(settings.URL + 'get_results/' + str(test_id),
                        auth=(settings.USR, settings.PWD), headers=headers)
+    return req.json()
+
+def get_results_for_case(run_id, case_id):
+    """Returns a list of test results for a test run and case combination"""
+    req = requests.get(settings.URL + '/%d/%d' % (run_id, case_id),
+                       auth=(settings.USR, settings.PWD), headers=headers)
+    return req.json()
+
+def get_suite(suite_id):
+    """Returns an existing test suite."""
+    req = requests.get(settings.URL + 'get_suite/' + str(suite_id),
+                       auth=(settings.USR, settings.PWD), headers=headers)
+    return req.json()
+
+def get_suites(project_id):
+    """Returns a list of test suites for a project"""
+    req = requests.get(settings.URL + 'get_suites/' + str(project_id),
+                       auth=(settings.USR, settings.PWD), headers=headers)
+
     return req.json()
